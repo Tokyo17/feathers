5726d808cfaf8bcc09349126451df203eb217aae
mRezaMustofa <reza.mustofa@ist.id>
2024-01-27 11:40:00 +0700

    f

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ const host = app.get('host')
 process.on('unhandledRejection', (reason) => logger.error('Unhandled Rejection %O', reason))
 
 app.listen(port).then(() => {
-  logger.info(`Feathers app listening on http://${host}:${port}`)
+  logger.info(`Feathers apps listening on http://${host}:${port}`)
 })
